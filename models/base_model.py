db371a03a8f1959bbd3bd1f78c9c590d7ef16809
oodafeisrael <oodafeisrael@yahoo.com>
2023-12-14 03:40:17 +0100

    task update

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,7 @@ class BaseModel:
         if kwargs:
             for key, val in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    val = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, val)
         else:
